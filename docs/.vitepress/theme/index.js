e0bbe9ec5eef8eb11fdec578e174a9b41747b0ec
JiaZhiheng <99702914+JiaZhiheng@users.noreply.github.com>
2022-10-07 21:40:24 +0800

    完善轮播组件

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,15 +2,11 @@ import Theme from 'vitepress/theme'
 import './style/index.css'
 import Home from './components/home.vue'
 
-import TDesign from 'tdesign-vue-next';
-import 'tdesign-vue-next/es/style/index.css';
-
-
-
 export default {
   ...Theme,
   enhanceApp({ app }) {
     app.component('Home', Home);
-    app.use(TDesign);
   }
 }
+
+
