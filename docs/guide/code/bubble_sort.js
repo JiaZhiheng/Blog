4cd401b3611f33f893afb4ae1ade711a7384d3e9
JiaZhiheng <99702914+JiaZhiheng@users.noreply.github.com>
2023-06-17 03:02:13 +0800

    update bubble_sort.js

---
+++ docs/guide/code/bubble_sort.js
@@ -0,0 +1,32 @@
+// bubble sore algorithm
+function bubbleSort(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = 0; j < arr.length - i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+			}
+		}
+	}
+	return arr;
+}
+
+// 优化，在没有发生交换的时候，停止循环
+function bubbleSort(arr) {
+	let swapped;
+	for (let i = 0; i < arr.length; i++) {
+		swapped = false;
+		for (let j = 0; j < arr.length - i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+				swapped = true;
+			}
+		}
+		console.log(arr);
+		if (!swapped) break;
+	}
+	return arr;
+}
+
+let arr = [3, 1, 5, 9, 6, 2];
+
+console.log(bubbleSort(arr));
