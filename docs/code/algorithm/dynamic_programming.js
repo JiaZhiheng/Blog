ada758fc624ca7970063a3ea2e92d3752a77bbd9
JiaZhiheng <99702914+JiaZhiheng@users.noreply.github.com>
2023-06-18 00:58:47 +0800

    update dynamic_programming.js

---
+++ docs/code/algorithm/dynamic_programming.js
@@ -0,0 +1,16 @@
+function fib(n) {
+	if (n <= 1) {
+		return n;
+	}
+	return fib(n - 1) + fib(n - 2);
+}
+
+function fib(n) {
+	const fib = [0, 1];
+	for (let i = 2; i <= n; i++) {
+		fib[i] = fib[i - 1] + fib[i - 2];
+	}
+	return fib[n];
+}
+
+console.log(fib(9));
---
+++ docs/code/algorithm/sliding_window.js
@@ -0,0 +1,47 @@
+for (let i = 0; i < arr.length - windowSize + 1; i++) {
+	for (let j = 0; j < windowSize; j++) {}
+}
+
+for (let i = 0; i < windowSize; i++) {}
+
+for (let i = windowSize; i < arr.length; i++) {}
+
+let arr = [3, 2, -4, 6, 1];
+
+function burteForce(arr, windowSize) {
+	if (arr.length < windowSize) {
+		return null;
+	}
+	let maxSum = 0;
+	for (let i = 0; i < arr.length - windowSize + 1; i++) {
+		let tempSum = 0;
+		for (let j = 0; j < windowSize; j++) {
+			tempSum += arr[i + j];
+		}
+		maxSum = Math.max(maxSum, tempSum);
+	}
+	return maxSum;
+}
+
+function slidingWindow(arr, windowSize) {
+	if (arr.length < windowSize) {
+		return null;
+	}
+
+	let maxSum = 0;
+	let tempSum = 0;
+
+	for (let i = 0; i < windowSize; i++) {
+		maxSum += arr[i];
+	}
+
+	tempSum = maxSum;
+	for (let i = windowSize; i < arr.length; i++) {
+		tempSum = tempSum - arr[i - windowSize] + arr[i];
+		maxSum = Math.max(maxSum, tempSum);
+	}
+
+	return maxSum;
+}
+
+console.log(slidingWindow([3, 2, -4, 6, 1], 3));
