0d41a2e17532d90f4299f6d3cb5f44a64117358f
JiaZhiheng <99702914+JiaZhiheng@users.noreply.github.com>
2023-09-04 15:21:16 +0800

    更新文件树计算函数

--- docs/code/generate_directory_tree.js
+++ docs/code/generate_directory_tree.js
@@ -2,11 +2,11 @@ import fs from "fs";
 import path from "path";
 
 const IGNORED_DIRECTORIES = [
-	'.git',
-	'.github',
-	'cache',
-	'dist',
-	'node_modules',
+  '.git',
+  '.github',
+  'cache',
+  'dist',
+  'node_modules',
   'card',
   'carousel',
   'hero',
@@ -14,63 +14,94 @@ const IGNORED_DIRECTORIES = [
   'util',
   'algorithm',
   'sort',
-	"01-HTML",
-	"02-CSS",
-	"03-JavaScript",
-	"04-Vue",
-	"05-React",
-	"06-Node.js",
-	"07-构建工具",
-	"08-单元测试",
-	"09-设计模式",
-	"10-计算机网络",
-	"11-浏览器原理",
-	"12-数据结构与算法",
+  "01-HTML",
+  "02-CSS",
+  "03-JavaScript",
+  "04-Vue",
+  "05-React",
+  "06-Node.js",
+  "07-构建工具",
+  "08-单元测试",
+  "09-设计模式",
+  "10-计算机网络",
+  "11-浏览器原理",
+  "12-数据结构与算法",
   'material',
   'project',
   'work',
   'document',
   'features',
   'project',
-]; // 目录名列表，你可以根据需要添加其他要忽略的目录
+];
 
 function generateDirectoryTree(
-	dirPath,
-	indent = "",
-	isLast = true,
-	parentIgnored = false
+  dirPath,
+  indent = "",
+  isLast = true,
+  parentIgnored = false
 ) {
-	const stats = fs.statSync(dirPath);
-	const prefix = isLast ? "└─ " : "├─ ";
-	const dirName = path.basename(dirPath);
-	const isIgnored = IGNORED_DIRECTORIES.includes(dirName);
+  const stats = fs.statSync(dirPath);
+  const prefix = isLast ? "└─ " : "├─ ";
+  const dirName = path.basename(dirPath);
+  const isIgnored = IGNORED_DIRECTORIES.includes(dirName);
 
-	if (isIgnored && !parentIgnored) {
-		console.log(`${indent}${prefix}${dirName}`);
-		return;
-	}
+  if (dirName !== "Blog" && dirName !== ".DS_Store") {
+    const symbol = stats.isDirectory() ? "📁 " : "📄 ";
+    console.log(`${indent}${prefix}${symbol}${dirName}`);
+  } else if (dirName === "Blog") {
+    console.log(`${indent}${dirName}`);
+  }
 
-	if (!isIgnored) {
-		console.log(`${indent}${prefix}${dirName}`);
-	}
+  if (isIgnored && !parentIgnored) {
+    return;
+  }
 
-	if (stats.isDirectory() && !isIgnored) {
-		const files = fs.readdirSync(dirPath);
+  if (stats.isDirectory() && !isIgnored) {
+    const files = fs.readdirSync(dirPath);
+    const directories = [];
+    const filesList = [];
 
-		for (let i = 0; i < files.length; i++) {
-			const file = files[i];
-			const filePath = path.join(dirPath, file);
-			const isLastItem = i === files.length - 1;
-			const subIndent = isLast ? "   " : "│  ";
-			const nextParentIgnored = parentIgnored || isIgnored;
-			generateDirectoryTree(
-				filePath,
-				`${indent}${subIndent}`,
-				isLastItem,
-				nextParentIgnored
-			);
-		}
-	}
+    // 分离文件夹和文件
+    for (const file of files) {
+      if (file !== ".DS_Store") {
+        const filePath = path.join(dirPath, file);
+        const fileStats = fs.statSync(filePath);
+        if (fileStats.isDirectory()) {
+          directories.push(filePath);
+        } else {
+          filesList.push(filePath);
+        }
+      }
+    }
+
+    // 先输出文件夹
+    for (let i = 0; i < directories.length; i++) {
+      const directory = directories[i];
+      const isLastItem = i === directories.length - 1;
+      const subIndent = isLast ? "   " : "│  ";
+      const nextParentIgnored = parentIgnored || isIgnored;
+      generateDirectoryTree(
+        directory,
+        `${indent}${subIndent}`,
+        isLastItem,
+        nextParentIgnored
+      );
+    }
+
+    // 再输出文件
+    for (let i = 0; i < filesList.length; i++) {
+      const file = filesList[i];
+      const isLastItem = i === filesList.length - 1;
+      const subIndent = isLast ? "   " : "│  ";
+      const nextParentIgnored = parentIgnored || isIgnored;
+      generateDirectoryTree(
+        file,
+        `${indent}${subIndent}`,
+        isLastItem,
+        nextParentIgnored
+      );
+    }
+  }
 }
 
 const targetDirectory = "../../../Blog";
