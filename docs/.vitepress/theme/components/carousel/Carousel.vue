2c5fcfc49059c87ee566d16c752831d0aed78b34
JiaZhiheng <99702914+JiaZhiheng@users.noreply.github.com>
2024-02-08 13:28:15 +0800

    修复浏览器重新回到标签页后卡片重叠的bug

--- docs/.vitepress/theme/components/carousel/Carousel.vue
+++ docs/.vitepress/theme/components/carousel/Carousel.vue
@@ -95,13 +95,52 @@ const cardNum = useSlots().default()[0].children.length;
 const indexCounter = ref(0);
 const playIntervalId = ref(null);
 
+const waitingForPlay = ref(false);
+/**
+ * 轮播图自动播放锁
+ * @type {Ref<boolean>}
+ * @default false
+ * @description 
+ * 作用：控制轮播图是否可以自动播放，当页面不可见时，自动播放会被暂停。
+ * 原因：由于浏览器的性能优化策略，当页面不可见时，浏览器会停止一切与页面渲染相关的操作。
+ *      此时 transition 动画停止播放但定时器仍在运行，造成动画与定时器不同步的现象。
+ *      当页面重新可见时，浏览器会把前面累计的动画效果全部执行，造成轮播图卡片位置混乱的问题。
+ * 目的：解决轮播图卡片位置混乱的问题。
+ * 用法：当轮播图需要切换且页面不可见时，将其赋值为 true 视作轮播图 「等待播放」
+ *      轮播图 「等待播放」 时回到标签页会调用 init 方法，此时轮播图继续播放。
+ * 注意：不要在页面不可见时将 waitingForPlay 赋值为 true。
+ *      因为频繁切换标签页时会重复调用 init 方法，导致轮播图重复播放，最终造成卡片重叠的问题。
+ *      而要在轮播图切换时将 waitingForPlay 赋值为 true。
+ *      即使频繁切换标签页也不会重复调用 init 方法，可以规避轮播图重复播放造成卡片重叠的问题。 
+ */
+
+// 监听页面可见性变化
+function handleVisibilityChange() {
+  if (!document.hidden && waitingForPlay.value) {
+    waitingForPlay.value = false;
+    init();
+  }
+}
+
+// 检查可见性
+function checkVisibility() {
+  if (document.hidden) {
+    waitingForPlay.value = true;
+    return false; // 表示不可见
+  } else {
+    return true; // 表示可见
+  }
+}
+
 // 滑动至前一页 (自动)
 function toPrev() {
+  if (!checkVisibility()) return; // 如果不可见，直接返回
   indexCounter.value = (cardNum - getCurrentIndex() + 1) % cardNum;
 }
 
 // 滑动至后一页 (自动)
 function toNext() {
+  if (!checkVisibility()) return; // 如果不可见，直接返回
   indexCounter.value = cardNum - getCurrentIndex() - 1;
 }
 
@@ -144,11 +183,18 @@ function init() {
   startPlay();
 }
 
+// 监听页面加载完成事件
 onMounted(() => {
   setTimeout(init, 1400);
+  document.addEventListener('visibilitychange', handleVisibilityChange);
 });
 
+// 监听页面卸载事件
 onUnmounted(() => {
   stopPlay();
+  document.removeEventListener('visibilitychange', handleVisibilityChange);
 });
 </script>
+
+
+
